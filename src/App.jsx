337fb37b2e4f028362fe1b45ebdef5a5917d79c5
Nakib00 <2022142@iub.edu.bd>
2025-03-23 13:48:33 +0600

    add title and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import GeneratorTab from './components/GeneratorTab';
-
+import GeneratorTab from "./components/GeneratorTab";
 
 const App = () => {
   return (
     <div className="app-container">
-
+      <h1>Nap QR</h1>
       <main className="main-content">
         <GeneratorTab />
       </main>
-
+      <p>Developed by Napver.com</p>
     </div>
   );
 };
